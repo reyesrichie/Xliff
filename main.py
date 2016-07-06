fd1a03988a9d4b60f64d187645ce96def9228e74
Ricardo Michel Reyes <me@richiereyes.com>
2016-07-06 13:19:19 -0700

    Read from disk, combine comments, and export to xml

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from xml.dom import minidom
+
 from Xliff import Xliff
 
 if __name__ == "__main__":
     filename = "fr.xliff"
     xliff = Xliff.read_from_disk(filename)
-
-    for file in xliff.files:
-        for unit in file.body.trans_units:
-            print unit
+    xliff.combine_translator_comments()
+    xliff.write_to_file('output.xml')
