ce5a1d7f43b14ebe969fed54c4f3a527c2e14195
Ricardo Michel Reyes <me@richiereyes.com>
2016-07-07 10:53:05 -0700

    Scan folder for files and save output to different folder

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from xml.dom import minidom
-
 from Xliff import Xliff
+import os
 
 if __name__ == "__main__":
-    filename = "fr.xliff"
-    xliff = Xliff.read_from_disk(filename)
-    xliff.combine_translator_comments()
-    xliff.write_to_file('output.xml')
+    folder = "/Users/Ricardo/Documents/Ariba"
+
+    for subdirectory, directory, files in os.walk(folder):
+        for file in files:
+            path = os.path.join(subdirectory, file)
+
+            filename, file_extension = os.path.splitext(path)
+            if file_extension == '.xliff':
+                xliff = Xliff.read_from_disk(path)
+                xliff.combine_translator_comments()
+
+                translator_dir = os.path.join(subdirectory, 'translator')
+                if not os.path.exists(translator_dir):
+                    os.makedirs(translator_dir)
+
+                xliff.write_to_file(os.path.join(translator_dir, file))
